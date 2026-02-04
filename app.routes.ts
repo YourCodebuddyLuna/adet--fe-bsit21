687f48dc38a60347c428ba77d6bfed5b3aa6594c
Nashville khent luna <nkluna69274@liceo.edu.ph>
2026-02-05 01:53:52 +0800

    Add files via upload

---
+++ app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PostsComponent } from './Posts/posts.component';
+
+export const routes: Routes = [
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
