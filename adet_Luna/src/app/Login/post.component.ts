c93ee2407a04712a66d5f99c5ba5591be0068f9c
Nashville khent luna <nkluna69274@liceo.edu.ph>
2026-02-05 19:39:48 +0800

    post.component.ts

--- adet_Luna/src/app/Login/post.component.ts
+++ adet_Luna/src/app/Login/post.component.ts
@@ -1,35 +1,22 @@
 import { Component } from '@angular/core';
 
-interface Post {
-  title: string;
-  description: string;
-}
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+    selector: 'app-posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
 
-  headerTitle = 'Community Feed';
 
-  posts: Post[] = [
-    {
-      title: 'Post Title',
-      description: 'This is a short description of the post content.'
-    },
-    {
-      title: 'Post Title',
-      description: 'Another example description for a post.'
-    },
-    {
-      title: 'Post Title',
-      description: 'More content shown inside the post card.'
+    posts = [
+        { title: 'Latest Updates', description: 'This is the first community post.' },
+        { title: 'Angular Tips', description: 'Learn how to build components properly.' },
+        { title: 'Announcements', description: 'New features coming soon!' }
+    ];
+
+
+    deletePost(index: number) {
+        this.posts.splice(index, 1);
     }
-  ];
-
-  deletePost(index: number): void {
-    this.posts.splice(index, 1);
-  }
 }
